865a592f002c9598e8bfb1fc6a0485a95a8e9357
JP27 <joanpomaresherrero@gmail.com>
2026-02-18 19:30:43 +0100

    refactorizacion de matches

--- routes/matches.js
+++ routes/matches.js
@@ -5,29 +5,29 @@ import { Match } from '../models/match.js';
 
 export const router = express.Router();
 
-router.get('/', (req, res) => {
-    Match.find()
-    .then((resultado) => {
-        if(resultado.length <= 0) {
-            res.statusCode = 404;
-            res.send("No existen jugadores registrados en el sistema");
+router.get('/', async (req, res) => {
+    try{
+        const match = await Match.find();
+
+        if(match.length <= 0) {
+            const error = Error()
+            error.name = "EmptyList"
+            throw error;
         }
-        else {
-            res.statusCode = 200;
-            res.send(resultado);
-        }
-    })
-    .catch((err) => {
-        res.statusCode = 500;
-        res.send("Error interno del servidor")
-    });
+
+        res.status(200).send(match)
+
+    } catch(ex) {
+        const error = getErrorMessage(ex);
+        res.status(error.statusCode).send(error.message);
+    }
 });
 
 
 router.get('/:id', async(req, res) => {
     try
     {
-        const id = req.params.id;
+        const { id } = req.params;
 
         const match = await Match.findById(id);
 
@@ -53,9 +53,6 @@ router.post('/', async (req, res) => {
             err.name = "SameTeams"
             throw err;
         }
-
-
-
 
         const savedMatch = await match.save();
         res.status(201).send(savedMatch)
@@ -88,18 +85,19 @@ router.delete('/:id', async (req, res) => {
 
 function getErrorMessage(er) {
     //console.log("errors: ", Object.values(er.errors).map(s => s.kind));
+    console.log(er)
 
+    if(er.name == 'EmptyList') { return { statusCode: 404, message: 'No existen partidos registrados.' } }
+
+    if(er.name == 'MatchNotFound') { return { statusCode: 404, message: 'Partido no encontrado.' } }
     
+    if(er.name == 'SameTeams') { return { statusCode: 400, message: 'El equipo local y visitante no pueden ser el mismo.' } }
 
-    if(er.name == 'AlreadyActive') { return { statusCode: 400, message: er.message } }
+    if(er.name == 'NeedParameters') { return { statusCode: 400, message: 'Falta el parámetro de búsqueda.' } }
     if(er.name == 'ValidationError') { return { statusCode: 400, message: 'Datos incorrectos: faltan campos obligatorios' } }
-    if(er.name == 'NotFound') { return { statusCode: 404, message: er.message } }
-    if(er.name == 'PlayerNotActive') { return { statusCode: 404, message: 'El jugador no está activo en el roster de este equipo.' } }
-    if(er.name == 'MatchNotFound') { return { statusCode: 404, message: 'Equipo no encontrado.' } }
-    if(er.name == 'SameTeams') { return { statusCode: 404, message: 'El equipo local y visitante no pueden ser el mismo.' } }
 
     // Si no es el error de falta de datos entonces con el codigo 11000 es el error de clave duplicada
-    if(er.code == 11000) { return { statusCode: 404, message: 'Ya existe este partido' } }
+    if(er.code == 11000) { return { statusCode: 404, message: 'El nickname ya está registrado' } }
     
     // Si no es ninguno de los anteriores el error debe ser del servidor
     return { statusCode: 500, message: 'Error interno del servidor' }
